88ef623ee9272badf2d65ce2f04f84af7a41a2e8
edwinyan <edwinyan@yeah.net>
2018-11-07 18:35:36 +0800

    update main.c

--- Firmware/main/main.c
+++ Firmware/main/main.c
@@ -17,19 +17,19 @@ OS_MUTEX	FIFO_MUTEX;
 #define  APP_CFG_TASK_START_STK_SIZE                    256u
 #define  APP_CFG_TASK_START_PRIO                        2u
 
-#define  APP_TX_TASK_STK_SIZE                    256u
-#define  APP_TX_TASK_PRIO                        3u
+#define  APP_RX_TASK_STK_SIZE                    256u
+#define  APP_RX_TASK_PRIO                        3u
 
 
 static  OS_TCB   app_task_start_tcb;
 static  CPU_STK  app_task_start_stk[APP_CFG_TASK_START_STK_SIZE];
 
-static  OS_TCB   app_tx_task_tcb;
-static  CPU_STK  app_tx_task_stk[APP_TX_TASK_STK_SIZE];
+static  OS_TCB   app_rx_task_tcb;
+static  CPU_STK  app_rx_task_stk[APP_RX_TASK_STK_SIZE];
 
 /*----------------------------------------------------------------------------*/
 //local function
-STATIC void app_rfm_tx_task(void *p_arg)
+STATIC void app_wol_io_rx_task(void *p_arg)
 {
 	OS_ERR      err;
 
@@ -48,14 +48,14 @@ STATIC void app_task_create(void)
 
 	OS_ERR      err;
 #if 1	
-    OSTaskCreate((OS_TCB       *)&app_tx_task_tcb,              
-                 (CPU_CHAR     *)"App rmf tx Task",
-                 (OS_TASK_PTR   )app_rfm_tx_task, 
+    OSTaskCreate((OS_TCB       *)&app_rx_task_tcb,              
+                 (CPU_CHAR     *)"App wol io rx Task",
+                 (OS_TASK_PTR   )app_wol_io_rx_task, 
                  (void         *)0,
-                 (OS_PRIO       )APP_TX_TASK_PRIO,
-                 (CPU_STK      *)&app_tx_task_stk[0],
-                 (CPU_STK_SIZE  )app_tx_task_stk[APP_TX_TASK_STK_SIZE / 10],
-                 (CPU_STK_SIZE  )APP_TX_TASK_STK_SIZE,
+                 (OS_PRIO       )APP_RX_TASK_PRIO,
+                 (CPU_STK      *)&app_rx_task_stk[0],
+                 (CPU_STK_SIZE  )app_rx_task_stk[APP_RX_TASK_STK_SIZE / 10],
+                 (CPU_STK_SIZE  )APP_RX_TASK_STK_SIZE,
                  (OS_MSG_QTY    )0,
                  (OS_TICK       )0,
                  (void         *)0,
@@ -90,7 +90,7 @@ STATIC void app_task_start(void *p_arg)
 #ifdef CPU_CFG_INT_DIS_MEAS_EN
     CPU_IntDisMeasMaxCurReset();
 #endif
-    MSG("Creating Application Tasks: %d\r\n",__FPU_USED);
+    //MSG("Creating Application Tasks: %d\r\n",__FPU_USED);
 	OSMutexCreate((OS_MUTEX*	)&FIFO_MUTEX,
 				  (CPU_CHAR*	)"UART FIFO_MUTEX",
 				  (OS_ERR*		)&err);
